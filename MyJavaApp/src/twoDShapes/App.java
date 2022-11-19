b6397ab61f7044a799bb96314ee9b2236d314013
Sahithi Narina <2021sp93084@wilp.bits-pilani.ac.in>
2022-11-19 12:14:08 +0530

    "Adding an abstract 2D Shape class"

---
+++ MyJavaApp/src/twoDShapes/App.java
@@ -0,0 +1,11 @@
+package twoDShapes;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Rectangle object= new Rectangle(4, 6);
+        object.perimeter();
+        Triangle object1= new Triangle(4,3,6);
+        object1.surfaceArea();
+
+    }
+}
---
+++ MyJavaApp/src/twoDShapes/Triangle.java
@@ -0,0 +1,29 @@
+package twoDShapes;
+
+public class Triangle extends Closed2DShapes{
+
+private double side1;
+private double side2;
+private double side3;	
+
+Triangle(double side_a, double side_b, double side_c)
+{
+	side1=side_a;
+	side2=side_b;
+	side3=side_c;
+}
+
+public void surfaceArea()
+	{
+	double area=0;
+	double s= (side1+side2+side3)/2;
+	area= s*(s-side1)*(s-side2)*(s-side3);
+	System.out.println("Surface Area is: "+ area);	
+	}	
+public void perimeter()
+	{
+	double peri=0;
+	peri = side1+side2+side3;
+	System.out.println("Perimeter is: "+ peri);		
+	}
+}
